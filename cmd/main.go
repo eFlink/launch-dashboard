c17ee1aaad412361041d116c0fe02b6b612e80e4
eFlink <erik.putu.permata.flink@gmail.com>
2024-04-11 23:32:30 +1000

    adds cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/eFlink/launch-dashboard-server/controller"
 	"github.com/eFlink/launch-dashboard-server/initializer"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +16,18 @@ func init() {
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	// Launch Endpoints
 	r.GET("/api/launches", controller.GetAll)
